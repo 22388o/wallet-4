0f7a1d913c4ce84289b9fa74fa7960e771277986
Mike Dedys <mdedys@users.noreply.github.com>
2022-09-06 16:45:24 -0400

    Add react + root render file

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+
+function render() {
+  const container = document.getElementById("root");
+
+  if (!container) {
+    console.error(`No element with ID: ${container} is found. No rendering react tree`);
+    return;
+  }
+
+  const root = createRoot(container);
+  root.render(<div>Hello World</div>);
+}
+
+render();
